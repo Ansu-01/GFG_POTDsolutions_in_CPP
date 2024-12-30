0f2d4351597e38526dc623ad92b0b6385d989d14
Ansu-01 <ansunanda122@gmail.com>
2024-12-30 10:57:57 +0530

    Solution for the day 30 Dec, 2024

---
+++ Union of Arrays with Duplicates.cpp
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template in C++
+
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+    int findUnion(vector<int>& a, vector<int>& b) {
+        // code here
+
+        int n1 = a.size();
+        int n2 = b.size();
+
+ 
+        unordered_map<int, int> hashh;
+        int i = 0;
+        while(i < n1){
+            hashh[a[i]]++;
+            i++;
+        }
+        i = 0;
+        while(i < n2) {
+            hashh[b[i]]++;
+            i++;
+        }
+        
+        int count = 0;
+        for(auto it : hashh) count++;
+        return count;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after reading t
+
+    while (t--) {
+        vector<int> a;
+        vector<int> b;
+
+        string input;
+        // For a
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // For b
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findUnion(a, b) << endl;
+        cout << '~' << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
